b5e8dd98576ba831d5b943693839cfa53a081b2e
Ahsan855 <a.r.ahsanhabib9869@gmail.com>
2023-10-04 18:36:48 +0600

    added some code

--- src/Pages/Home/Bannar.js
+++ src/Pages/Home/Bannar.js
@@ -1,7 +1,11 @@
-import React from "react";
 import bannarImage from "../../images/Bannar_Live_chat.svg";
+import React, { useState } from "react";
 
 const Bannar = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
   return (
     <div>
       <div>
@@ -13,6 +17,7 @@ const Bannar = () => {
               <h1 className="sm:text-3xl md:text-2xl sm:px-5 lg:text-6xl text-center lg:text-left lg:leading-[68px] text-primary font-bold">
                 Read the most <br /> interesting articles
               </h1>
+
               <p className="leading-[36px]  text-[20px] text-center lg:text-left text-primary font-normal sm:px-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua
@@ -27,17 +32,32 @@ const Bannar = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
                     d="M15.7781 14.8968L14.8937 15.7812C14.6031 16.075 14.1281 16.075 13.8343 15.7812L10.7187 12.6656C10.5781 12.525 10.5 12.3344 10.5 12.1343V11.625C9.39686 12.4875 8.00935 13 6.49998 13C2.90936 13 0 10.0906 0 6.49998C0 2.90936 2.90936 0 6.49998 0C10.0906 0 13 2.90936 13 6.49998C13 8.00935 12.4875 9.39686 11.625 10.5H12.1343C12.3344 10.5 12.525 10.5781 12.6656 10.7187L15.7812 13.8343C16.0718 14.1281 16.0718 14.6031 15.7781 14.8968ZM2.49999 6.49998C2.49999 8.71247 4.29061 10.5 6.49998 10.5C8.71247 10.5 10.5 8.70936 10.5 6.49998C10.5 4.2875 8.70936 2.49999 6.49998 2.49999C4.2875 2.49999 2.49999 4.29061 2.49999 6.49998Z"
                     fill="#959EAD"
                   />
                 </svg>
-                <input
-                  type="text"
-                  placeholder="Search article"
-                  className=" input input-bordered hover:border-blue-300 w-full pl-12 italic font-bold leading-[21.79px] "
-                />
+
+                <div className="relative">
+                  <input
+                    type="text"
+                    className="border border-gray-300 rounded px-3 py-2 outline-none focus:border-blue-500 input input-bordered hover:border-blue-300 w-full pl-12 italic font-bold leading-[21.79px] "
+                    onClick={toggleMenu}
+                    placeholder="Search article"
+                  />
+
+                  {isMenuOpen && (
+                    <div className="absolute mt-2 bg-white text-primary rounded shadow-lg">
+                      {/* Your menu items go here */}
+                      <ul>
+                        <li className="p-2">Article 1</li>
+                        <li className="p-2">Article 2</li>
+                        <li className="p-2">Article 3</li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
 
                 <button className="w-full px-5 sm:text-center md:w-[94px] h-[40px] bg-[#1565D8] text-[16px] font-bold text-white mt-5 md:absolute md:mt-0 top-1 right-2 rounded-lg">
                   Search
