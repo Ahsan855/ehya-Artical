d181d8f2654c0c60b2c92e6881cbe02b29f8cf28
Ahsan855 <a.r.ahsanhabib9869@gmail.com>
2023-10-06 22:19:45 +0600

    footer responsive done

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -7,86 +7,95 @@ const Footer = () => {
   return (
     <div className="bg-[#0D2436] py-10">
       <div className="border-[#1A3245] w-11/12 mx-auto border-b-2"></div>
-      <footer className="flex flex-row gap-x-16 container mx-auto flex-wrap pt-16 px-5 md:px-10  text-[#959EAD]">
-        <div>
-          <img src={logo} alt="" />
-          <p className="my-5">
-            Build a modern and creative
-            <br />
-            website with crealand
+      <div className="flex md:flex-row flex-col-reverse items-center justify-center lg:gap-x-20 max-w-7xl mx-auto">
+        <div className="py-5 md:py-0 text-center px-10">
+          <img className="w-1/2 mx-auto md:mx-0" src={logo} alt="" />
+          <p className="my-5 md:text-[16px] text-center w-11/12 mx-auto md:mx-0 md:text-left text-[#5A7184]">
+            Build a modern and creative website with crealand
           </p>
-          <div className="flex flex-row gap-2">
+          <div className="flex justify-center md:justify-start flex-row gap-2">
             <img src={cercleIcon} alt="" />
             <img src={cercleIcon} alt="" />
             <img src={cercleIcon} alt="" />
             <img src={cercleIcon} alt="" />
           </div>
         </div>
-        <div>
-          <header className="font-bold  text-lg text-[#5A7184]">Product</header>
-          <li className="list-none py-2">
-            <a className="link link-hover">Landingpage</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Features</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Documentation</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Referal Program</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Pricing</a>
-          </li>
-        </div>
-        <div>
-          <header className="font-bold  text-lg text-[#5A7184]">Service</header>
-          <li className="list-none py-2">
-            <a className="link link-hover">Documentation</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Design</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Themes</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">illustrations</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">UI Kit</a>
-          </li>
-        </div>
+        <div className="flex flex-wrap md:flex-row lg:flex-row gap-x-10 lg:gap-x-28 container mx-auto pt-16 px-10 md:px-10  text-[#959EAD]">
+          <div>
+            <header className="font-bold py-5 text-lg text-[#5A7184]">
+              Product
+            </header>
+            <li className="list-none py-2">
+              <a className="link link-hover">Landingpage</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Features</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Documentation</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Referal Program</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Pricing</a>
+            </li>
+          </div>
+          <div>
+            <header className="font-bold py-5  text-lg text-[#5A7184]">
+              Service
+            </header>
+            <li className="list-none py-2">
+              <a className="link link-hover">Documentation</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Design</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Themes</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">illustrations</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">UI Kit</a>
+            </li>
+          </div>
 
-        <div>
-          <header className="font-bold text-lg text-[#5A7184]">Company</header>
-          <li className="list-none py-2">
-            <a className="link link-hover">About</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Terms</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Privacy Policy</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Careers</a>
-          </li>
+          <div>
+            <header className="font-bold py-5 text-lg text-[#5A7184]">
+              Company
+            </header>
+            <li className="list-none py-2">
+              <a className="link link-hover">About</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Terms</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Privacy Policy</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Careers</a>
+            </li>
+          </div>
+          <div>
+            <header className="font-bold py-5 text-lg text-[#5A7184]">
+              More
+            </header>
+            <li className="list-none py-2">
+              <a className="link link-hover">Documentation</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">License</a>
+            </li>
+            <li className="list-none py-2">
+              <a className="link link-hover">Chagelog</a>
+            </li>
+          </div>
         </div>
-        <div>
-          <header className="font-bold text-lg text-[#5A7184]">More</header>
-          <li className="list-none py-2">
-            <a className="link link-hover">Documentation</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">License</a>
-          </li>
-          <li className="list-none py-2">
-            <a className="link link-hover">Chagelog</a>
-          </li>
-        </div>
-      </footer>
+      </div>
+
       <div className="text-center pt-5 bg-[#0D2436] text-[#959EAD]">
         <img className="mx-auto my-5" src={loveImage} alt="" />
         <span>Copyright © 2023. Crafted with love.</span>
