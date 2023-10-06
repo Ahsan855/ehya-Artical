ff72dd3b838f03f60cbf41c9fcc072c2780048ba
Ahsan855 <a.r.ahsanhabib9869@gmail.com>
2023-10-06 23:16:07 +0600

    footer top responsive add

--- src/Pages/Footer/FooterTop.js
+++ src/Pages/Footer/FooterTop.js
@@ -10,29 +10,31 @@ const FooterTop = () => {
       <div>
         <img className=" w-full h-auto" src={shape} alt="" />
         <div className="bg-[#0D2436]">
-          <section className=" py-10 container mx-auto px-5  flex flex-col md:flex-row md:flex-wrap">
-            <div className="w-1/2">
-              <h1 className="text-white font-bold py-5 text-4xl">
+          <section className=" py-10 container mx-auto px-5 flex md:flex-col-reverse flex-col lg:flex-row md:flex-wrap">
+            <div className="lg:w-1/2 mt-36 lg:mt-5  mx-auto">
+              <h1 className="text-white text-center lg:text-left font-bold py-5 text-4xl">
                 Get our stories delivered From us <br /> to your inbox weekly.
               </h1>
-              <div className="flex flex-row flex-wrap gap-2 my-10 w-full sm:w-96">
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="Your email"
-                  className="border rounded-md py-2 w-full md:w-auto px-3 focus:outline-none focus:border-blue-500"
-                />
-                <button className="px-5 py-2 w-full md:w-auto hover:border-blue-500 hover:border-2  text-white hover:text-blue-500 text-sm font-bold bg-[#1565D8] hover:bg-white transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg rounded-md">
-                  Get Started
-                </button>
+              <div className="">
+                <div className="flex md:flex-row justify-center lg:justify-start items-center gap-2 flex-col flex-wrap my-5">
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Your email"
+                    className="border rounded-md py-2 w-full md:w-auto px-10 focus:outline-none focus:border-blue-500"
+                  />
+                  <button className=" px-5 py-2 w-full md:w-auto  hover:text-[#1565D8] hover:bg-white bg-[#1565D8] text-[16px] font-bold text-white rounded-lg">
+                    Get Started
+                  </button>
+                </div>
+                <h1 className=" text-xl px-5 py-5 text-center lg:text-left text-[#5A7184] font-bold">
+                  Get a response tomorrow if you submit by 9pm today. If we
+                  received after 9pm will get a reponse the following day.
+                </h1>
               </div>
-              <p className="text-[#5A7184] text-[16px] w-1/2">
-                Get a response tomorrow if you submit by 9pm today. If we
-                received after 9pm will get a reponse the following day.
-              </p>
             </div>
-            <div className="relative z-10 w-1/2 hidden md:block">
+            <div className="relative z-10 lg:w-1/2 hidden md:block">
               <div className="bg-white p-2  rounded-3xl shadow-md">
                 <img className="w-full h-auto" src={payOn} alt="" />
                 <h2 className="text-2xl font-bold text-primary py-5 px-5">
